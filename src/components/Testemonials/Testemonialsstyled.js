67c6779a14df7603bd07d6431f6f737dce2728b2
Lorenzo Taylor <57335062+LorenzoTaylor@users.noreply.github.com>
2024-06-17 10:48:33 -0700

    Update Testemonialsstyled.js

--- src/components/Testemonials/Testemonialsstyled.js
+++ src/components/Testemonials/Testemonialsstyled.js
@@ -43,7 +43,7 @@ export const TestemonialImage = styled.img`
     align-self: center;
     height: 300px;
     width: 100%;
-    max-width: 100%;
+    max-width: 80px;
 `;
 
 export const ResponsiveDiv = styled.div`
